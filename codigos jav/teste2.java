fc92f0da711d90011f4e9544354e77e84bad96db
Marzano <80858692+Maarzano@users.noreply.github.com>
2024-10-09 15:51:36 -0300

    foi dificil resolver isso

--- codigos jav/teste2.java
+++ codigos jav/teste2.java
@@ -1,5 +1,5 @@
 public class teste2 {
     public static void main(String[] args) {
-        System.out.println("vai tomar no cu porra!");
+        System.out.println("foi difícil resolver isso...!");
     }
 }
