afdf3eb1e21dec050a116e6e1d2513883a637137
Marzano <arthuramarzano10@gmail.com>
2024-10-09 15:47:25 -0300

    resolvendo problema com o Java

---
+++ codigos jav/teste2.java
@@ -0,0 +1,5 @@
+public class teste2 {
+    public static void main(String[] args) {
+        System.out.println("vai tomar no cu porra!");
+    }
+}
--- codigos java/Teste.java
+++
@@ -1,7 +0,0 @@
-package codigos java;
-
-public class Teste {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
