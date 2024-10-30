4f4d3777921744044c4c12e74c475fe023b75ac2
Marzano <arthuramarzano10@gmail.com>
2024-10-30 09:51:07 -0300

    criando projeto de Java para estudar lógica

--- codigos jav/Lógica_Java/src/teste.java
+++ codigos jav/Lógica_Java/src/teste.java
@@ -1,4 +1,5 @@
-public class teste2 {
+package Lógica_Java.src;
+public class teste {
     public static void main(String[] args) {
         System.out.println("foi difícil resolver isso...!");
     }
